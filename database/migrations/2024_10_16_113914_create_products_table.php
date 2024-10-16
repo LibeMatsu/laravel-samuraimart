cd5b03b7c5a87db1d2cd6646c0637c5a4d2d8461
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-10-16 11:49:39 +0900

    CategoryとProductを紐づけ

--- database/migrations/2024_10_16_113914_create_products_table.php
+++ database/migrations/2024_10_16_113914_create_products_table.php
@@ -19,6 +19,10 @@ return new class extends Migration
             $table->text('description');
             $table->integer('price')->unsigned();
             // ↑unsigned()を追加することで、-5000などのマイナスの値が保存できないようになる
+            $table->integer('category_id')->unsigned();
+            // ↑Eloquentは外部キーカラムを自動的に決定します。
+            // 規約により、親モデルの小文字クラス名に「_id」という接尾辞を付けます。
+            // この場合では、Categoryモデルに対する外部キーカラムはcategory_idとなり、商品とカテゴリが紐づきます。
             $table->timestamps();
         });
     }
