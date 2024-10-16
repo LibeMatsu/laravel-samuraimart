af26f99925cd88a99977a1ef9929d8509ff9157c
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-10-16 14:25:50 +0900

    削除機能を追加

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -100,6 +100,8 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return to_route('products.index');
     }
 }
