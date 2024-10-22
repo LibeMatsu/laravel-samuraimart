bdcfa61e189c89e59e6c30332bb8715776310811
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-10-22 15:19:24 +0900

    一ページの表示件数を変更

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::paginate(15);
+        $products = Product::paginate(8);
 
         return view('products.index', compact('products'));
     }
