932656c4fb446e7779c57c0bc4006de6af20ee06
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-10-22 15:25:30 +0900

    ProductControllerでカテゴリーテーブルのカラムを取得してviewに渡す

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,8 +16,10 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::paginate(8);
+        $categories = Category::all();
+        $major_category_names = Category::pluck('major_category_name')->unique();
 
-        return view('products.index', compact('products'));
+        return view('products.index', compact('products', 'categories', 'major_category_names'));
     }
 
     /**
