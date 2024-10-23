44be57a0b328ce9cf2f22ebff015701cfe108b0e
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-10-23 10:44:10 +0900

    UserControllerのupdateアクションを編集

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,7 +35,16 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $user = Auth::user();
+
+        $user->name = $request->input('name') ? $request->input('name') : $user->name;
+        $user->email = $request->input('email') ? $request->input('email') : $user->email;
+        $user->postal_code = $request->input('postal_code') ? $request->input('postal_code') : $user->postal_code;
+        $user->address = $request->input('address') ? $request->input('address') : $user->address;
+        $user->phone = $request->input('phone') ? $request->input('phone') : $user->phone;
+        $user->update();
+
+        return to_route('mypage');
     }
 
 }
