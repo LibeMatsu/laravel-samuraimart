26bed292fddbfbb53b72297626e2d99f79d8c7a9
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-10-23 10:34:41 +0900

    次回からの新規登録時に増やした分のテーブルにもデータが入るようにUserモデルを編集

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $hidden = [
         'password',
         'remember_token',
+        'postal_code',
+        'address',
+        'phone'
     ];
 
     /**
