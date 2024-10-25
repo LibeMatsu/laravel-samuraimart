e56607a8e39e98f198919a39726fd5342ea89f39
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-10-25 13:36:55 +0900

    shoppingcartテーブルに購入済みかどうかを判定する為のカラムを追加

--- database/migrations/2024_10_25_133457_add_buy_flag_to_shoppingcart.php
+++ database/migrations/2024_10_25_133457_add_buy_flag_to_shoppingcart.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('shoppingcart', function (Blueprint $table) {
-            //
+            $table->boolean('buy_flag')->default(false);
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('shoppingcart', function (Blueprint $table) {
-            //
+            $table->dropColumn('buy_flag');
         });
     }
 };
