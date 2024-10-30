b7db25de49f8a108e191840dbf267ec393a35b9f
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-10-30 15:03:18 +0900

    categoriesテーブルでいらなくなったカラムを削除。のためのマイグレーションファイルを編集

--- database/migrations/2024_10_30_150145_drop_column_categories_table.php
+++ database/migrations/2024_10_30_150145_drop_column_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->dropColumn('major_category_name');
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->string('major_category_name');
         });
     }
 };
