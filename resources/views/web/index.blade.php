fd881345cdb5720809b1270db8463afc4fcd7619
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-10-31 09:49:50 +0900

    Web.index 新着商品を表示するための変更

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -57,61 +57,25 @@
             <a href="{{ route('products.index', ['sort' => 'id', 'direction' => 'desc']) }}">もっと見る</a>
          </div>
          <div class="row">
-             <div class="col-3">
-                 <a href="#">
-                     <img src="{{ asset('img/fksyabusameIMG_6086-341_TP_V4.jpg') }}" class="img-thumbnail">
-                 </a>
-                 <div class="row">
-                     <div class="col-12">
-                         <p class="samuraimart-product-label mt-2">
-                             信楽焼狸<br>
-                             <label>￥55000</label>
-                         </p>
-                     </div>
-                 </div>
-             </div>
- 
-             <div class="col-3">
-                 <a href="#">
-                     <img src="{{ asset('img/nekocyanPAKE5233-481_TP_V4.jpg') }}" class="img-thumbnail">
-                 </a>
-                 <div class="row">
-                     <div class="col-12">
-                         <p class="samuraimart-product-label mt-2">
-                             ソファー<br>
-                             <label>￥35000</label>
-                         </p>
-                     </div>
-                 </div>
-             </div>
- 
-             <div class="col-3">
-                 <a href="#">
-                     <img src="{{ asset('img/ogasuta458A8165_TP_V4.jpg') }}" class="img-thumbnail">
-                 </a>
-                 <div class="row">
-                     <div class="col-12">
-                         <p class="samuraimart-product-label mt-2">
-                             コーヒーカップ<br>
-                             <label>￥1000</label>
-                         </p>
-                     </div>
-                 </div>
-             </div>
- 
-             <div class="col-3">
-                 <a href="#">
-                     <img src="{{ asset('img/Tm2132038A3951_TP_V4.jpg') }}" class="img-thumbnail">
-                 </a>
-                 <div class="row">
-                     <div class="col-12">
-                         <p class="samuraimart-product-label mt-2">
-                             食器セット<br>
-                             <label>￥2000</label>
-                         </p>
-                     </div>
-                 </div>
-             </div>
+            @foreach ($recently_products as $recently_product)
+                <div class="col-3">
+                    <a href="{{ route('products.show', $recently_product) }}">
+                        @if ($recently_product->image !== "")
+                            <img src="{{ asset($recently_product->image) }}" class="img-thumbnail">
+                        @else
+                            <img src="{{ asset('img/dummy.png')}}" class="img-thumbnail">
+                        @endif
+                    </a>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="samuraimart-product-label mt-2">
+                                {{ $recently_product->name }}<br>
+                                <label>￥{{ $recently_product->price }}</label>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+             @endforeach
          </div>
      </div>
  </div>
