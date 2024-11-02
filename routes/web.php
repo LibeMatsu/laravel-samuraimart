a9a616bd40ef48e133d613a55dcf75da52b0dc03
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-11-02 11:05:36 +0900

    web.phpに記載漏れがあったので修正

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
