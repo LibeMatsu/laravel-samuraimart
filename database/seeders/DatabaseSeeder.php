885c58443a0c5a23d904ba34cdfb8e6de71bad06
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-11-06 10:21:21 +0900

    DatabaseSeederを編集

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call(MajorCategoriesSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+
+        $this->call(AdminUserSeeder::class);
     }
 }
